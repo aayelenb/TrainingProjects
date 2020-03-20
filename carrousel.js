48786dfe5031922beb2102f6876c0b2c7d31f916
Ayelen Barreto <abarreto@technisys.com>
2020-03-20 11:54:52 -0300

    Correcciones

--- carrousel.js
+++ carrousel.js
@@ -2,26 +2,26 @@ import React, {useState} from "react";
 import ManageForm from "./manageForm"
 
 
-function Carousel(props){
+function Carousel({children}){
 
     const [index, setIndex]=useState(0);
 
     const handleClickNext=()=>{
-            props.children.length!=index+1?setIndex(index+1):setIndex(0);
+            children.length!=index+1?setIndex(index+1):setIndex(0);
         }
     
     const handleClickBack=()=>{
-            index!=0?setIndex(index-1):setIndex(props.children.length-1);
+            index!=0?setIndex(index-1):setIndex(children.length-1);
     }
 
     return(
         <div align="center">
             <h4>Carousel challenge</h4>
-            {props.children[index]}  
+            {children[index]}  
                 <div>
                     <button onClick={handleClickBack}>Back</button>
                     <button onClick={handleClickNext}>Next</button>
-            </div>
+                </div>
         </div>
     )
 }
